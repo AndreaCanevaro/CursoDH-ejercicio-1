121fb88d10cab5d26b8b3cecd1503c7d91e09d59
AndreaCanevaro <andrea.canevaro.f@gmail.com>
2021-07-08 10:23:37 -0300

    Agrego una función al archivo 1

--- archivo1.js
+++ archivo1.js
@@ -1,1 +1,11 @@
 console.log ("Archivo 1");
+let diaDeLaSemana="Jueves";
+function costa (diaDeLaSemana){
+    
+    if ((diaDeLaSemana!="Lunes")||(diaDeLaSemana!="Martes")||(diaDeLaSemana!="Miércoles")){
+        console.log ("Viajar a la costa");
+    } else {
+        console.log("Quedarse en casa")
+    }
+};
+console.log (costa());
